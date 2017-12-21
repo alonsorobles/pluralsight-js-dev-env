dbe10ee1e0cbb617950b76f83ec55d00b1937710
Alonso Robles <alonso@alonsorobles.com>
2017-12-21 13:47:21 -0600

    Added logic to inject track js into production build only

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -43,7 +43,10 @@ export default {
         minifyCSS: true,
         minifyURLs: true,
       },
-      inject: true
+      inject: true,
+      // Properties you define here are available inindex.html
+      // using htmlWebpackPlugin.options.varName
+      trackJSToken: 'INSERT YOUR TOKEN HERE'
     }),
     // Eliminate duplicate pacakges when generating bundle
     new webpack.optimize.DedupePlugin(),
