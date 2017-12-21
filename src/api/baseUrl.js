bdee39a56afaeeb2d83517ab3b6c3bb27f458706
Alonso Robles <alonso@alonsorobles.com>
2017-12-21 14:26:51 -0600

    Updated getBaseUrl() to return heroku URL real API

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://immense-woodland-92338.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
